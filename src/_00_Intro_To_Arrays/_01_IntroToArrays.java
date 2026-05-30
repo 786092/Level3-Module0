cb554b9ad079d6eae58701832cd34792b07ac427
786092 <Irfaan.S7860@gmail.com>
2026-05-30 11:49:49 -0700

    Continuing intro to Array from Step 6

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -5,16 +5,26 @@ import java.util.Random;
 public class _01_IntroToArrays {
     public static void main(String[] args) {
         // 1. declare and Initialize an array 5 Strings
+        String[] lakers = new String[5];
 
+        lakers[0] = "Magic Johnson";
+        lakers[1] = "Shaq";
+        lakers[2] = "Jerry West";
+        lakers[3] = "LeBron James";
+        lakers [4] = "Luka Doncic";
         // 2. print the third element in the array
-
+        System.out.println(lakers[2]);
         // 3. set the third element to a different value
-
+        lakers[2] = "Kobe Bryant";
         // 4. print the third element again
-
+        System.out.println(lakers[2]);
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
+        String myChoice = "Black Mamba";
 
+        for (int i = 0; i < lakers.length; i++) {
+            lakers[i] = myChoice;
+        }
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
 
